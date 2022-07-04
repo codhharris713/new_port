ec88df8632b595b2582e9ff896a9a08538db15d7
codhharris713 <83795096+codhharris713@users.noreply.github.com>
2022-07-04 16:11:01 -0700

    fixed resume

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -63,7 +63,7 @@ export default function AboutMe(props) {
                 {" "}
                 Hire Me{" "}
               </button>
-              <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+              <a href="Cody_Harris.pdf" download="Cody Cody_Harris.pdf">
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
